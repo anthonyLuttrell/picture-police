d13d7929d929d0c0c9507b6455292ae32e61490d
anthonyLuttrell <anthonyluttrell@gmail.com>
2025-12-28 15:13:20 -0600

    backup

--- src/Match.ts
+++ src/Match.ts
@@ -44,10 +44,6 @@ export class Match
         // ignore partial matches when full matches exist. We will keep a list
         // of partial matches to add later if we don't find any full matches.
         const tempPartialMatches: string[] = [];
-
-        // FIXME there is an issue with the in-n-out picture not being id'd
-        // correctly, it seems to be using the partial match first. And the
-        // chocolate chip bread should be getting a match, but it is not.
         const matchingPages = this.matchingImagesObj;
 
         for (const page of matchingPages)
@@ -55,33 +51,36 @@ export class Match
             const hasFullMatch = page.fullMatchingImages !== undefined;
             const hasPartialMatch = page.partialMatchingImages !== undefined;
 
-            // Reddit will create new URLs with the query string "?tls=" to
-            // auto-translate a post to a different language. Don't add these.
-
-            // TODO use the URL object for parsing hostnames safely.
-            // try
-            // {
-            //     const urlObj = new URL(page.url);
-            //     const isReddit = urlObj.hostname.endsWith('reddit.com') || urlObj.hostname === 'redd.it';
-            // } catch (e) { /* invalid url */ }
-            const goodRedditUrl: boolean = (page.url.includes("reddit.com") ||
-                                           page.url.includes("redd.it") ||
-                                           page.url.includes("/comments/")) &&
-                                           !page.url.includes("/user/") &&
-                                           !page.url.includes("/?tl=");
-
-            const isExternal: boolean = !page.url.includes("reddit.com") &&
-                                        !page.url.includes("redd.it");
-
-            if (hasFullMatch && (goodRedditUrl || isExternal))
+            try
             {
-                console.debug(`\nAdding url to matchList: ${page.url}\n  fullMatch: ${hasFullMatch}\n  partialMatch: ${hasPartialMatch}\n  goodRedditUrl: ${goodRedditUrl}\n  isExternal: ${isExternal}\n`);
-                this.matchList.push(page.url);
+                const urlObj = new URL(page.url);
+                const path = urlObj.pathname;
+                const host = urlObj.hostname;
+                const proto = urlObj.protocol;
+                const notQuery = urlObj.search === "";
+
+                const isRedditPermalink = host.endsWith("reddit.com") &&
+                                          path.includes("comments") &&
+                                          notQuery;
+
+                const isExternal = !host.endsWith("reddit.com") &&
+                                   !host.includes("redd.it") &&
+                                   proto.includes("https");
+
+                if (hasFullMatch && (isRedditPermalink || isExternal))
+                {
+                    console.debug(`\nAdding url to matchList: ${page.url}\n  fullMatch: ${hasFullMatch}\n  partialMatch: ${hasPartialMatch}\n  goodRedditUrl: ${isRedditPermalink}\n  isExternal: ${isExternal}\n`);
+                    this.matchList.push(page.url);
+                }
+                else if (hasPartialMatch && (isRedditPermalink || isExternal))
+                {
+                    console.debug(`saving partial match`);
+                    tempPartialMatches.push(page.url);
+                }
             }
-            else if (hasPartialMatch && (goodRedditUrl || isExternal))
+            catch (e)
             {
-                console.debug(`saving partial match`);
-                tempPartialMatches.push(page.url);
+                console.error(e);
             }
         }
 
@@ -111,11 +110,6 @@ export class Match
         this.numCleanedMatches = this.matchList.length;
     }
 
-    /**
-     * Calculate the odds that OP is using a stolen image. For example, if we found
-     * 5 matching images, but OP has previously posted 4 of them, that would be
-     * 1 - (4 / 5) * 100, which is a 20% likelihood of stolen images.
-     */
     get score()
     {
         const matchDiff = this.numOriginalMatches - this.numCleanedMatches;
--- src/scan.ts
+++ src/scan.ts
@@ -2,44 +2,10 @@ import {checkGoogleVision} from "./gvis.js";
 import {getOpFromUrl} from "./utils.js";
 import {Match} from "./Match.js";
 
-/**
- * Performs a reverse image search using the provided Google Vision API key
- * and context, iterating over the given list of image URLs.
- *
- * @param {string} key - The API key to authenticate with the Google Vision API.
- * @param {string[]} sourceUrls - The list of image URLs to process in the reverse
- * search.
- * @return {Promise<Match[]|[]>} - A promise that resolves to an array of `Match`
- */
 export async function reverseImageSearch(
     key: string,
     sourceUrls: string[]): Promise<Match[]|[]>
 {
-    // const sourceMatches: Match[] = [];
-    //
-    // for (const sourceUrl of sourceUrls)
-    // {
-    //     console.debug(`Working image: ${sourceUrl}`);
-    //     const result = await checkGoogleVision(sourceUrl, key);
-    //
-    //     if (!result)
-    //     {
-    //         console.log("Bad result from Google Vision API.");
-    //         continue;
-    //     }
-    //
-    //     // create a new Match object for every picture in a post
-    //     // `pagesWithMatchingImages` may be undefined here, but that is okay
-    //     console.debug(`pagesWithMatchingImages exists: ${result.pagesWithMatchingImages !== undefined}`);
-    //     console.debug(`fullMatchingImages exists: ${result.fullMatchingImages !== undefined}`);
-    //     console.debug(`partialMatchingImages exists: ${result.partialMatchingImages !== undefined}`);
-    //     console.debug(`visuallySimilarImages exists: ${result.visuallySimilarImages !== undefined}`);
-    //     sourceMatches.push(
-    //         new Match(result.pagesWithMatchingImages, sourceMatches.length)
-    //     );
-    // }
-    // return sourceMatches;
-
     const promises = sourceUrls.map(async (url, index) =>
     {
         const result = await checkGoogleVision(url, key);
@@ -48,7 +14,6 @@ export async function reverseImageSearch(
     });
 
     const results = await Promise.all(promises);
-    // Filter out nulls
     return results.filter((m): m is Match => m !== null);
 }
 
@@ -79,21 +44,15 @@ export async function findMatchingUsernames(
             if ((url.includes("reddit.com") || url.includes("redd.it")) &&
                 url.includes("/comments/"))
             {   // find reddit URLs only
-                console.debug(`Checking for reddit OP in url: ${url}`);
                 const foundAuthor = await getOpFromUrl(url, context.reddit);
 
                 if (!foundAuthor)
                 {
-                    console.debug(
-                        `Unable to find a username from the provided url:\n  ${url}`
-                    );
                     continue;
                 }
 
                 if (authorName === foundAuthor)
                 {
-                    console.log(`Matched OP: u/${foundAuthor}`);
-                    console.debug(`Removing url: ${url}`);
                     urlsToRemove.push(url);
                 }
             }

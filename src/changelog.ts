89f5192c91ed9f63e932435840db5dbdc23b6ff9
Anthony <luttrell321@gmail.com>
2026-01-24 13:04:17 -0600

    chore: adding change notes

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,7 +1,8 @@
 export const APP_CHANGELOG: Record<string, string[]> =
 {
     "1.3.0": [
-        "Add a setting to send mod mail notifications when a new version of Picture Police is available",
+        "Add a setting to send mod mail notifications when a new version of Picture Police is published",
+        "Add a setting to exclude mods from all Picture Police scans",
         "Reduce false positives on partial matches when only direct Reddit image links are found",
         "Improve Facebook link matching",
         "Fix bug preventing action summaries from being sent"

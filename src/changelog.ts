7ac7ef37ecd91cdda5472ef2dbdea2fad46a6e5c
Anthony <luttrell321@gmail.com>
2026-01-25 13:56:51 -0600

    chore: adding release notes

--- src/changelog.ts
+++ src/changelog.ts
@@ -4,7 +4,8 @@ export const APP_CHANGELOG: Record<string, string[]> =
         "Add a setting to send mod mail notifications when a new version of Picture Police is published",
         "Add a setting to exclude mods from all Picture Police scans",
         "Reduce false positives on partial matches when only direct Reddit image links are found",
-        "Improve Facebook link matching",
-        "Fix bug preventing action summaries from being sent"
+        "Change all external (non-Reddit) website links to direct-image links",
+        "Fix bug preventing action summaries from being sent",
+        "Change OP whitelist mod menu item to a toggle and add app name to label"
     ],
 };

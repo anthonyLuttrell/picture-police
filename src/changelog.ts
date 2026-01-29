bebd2674a66ba69e470caec4994a1b25929bb91b
Anthony <luttrell321@gmail.com>
2026-01-29 15:04:17 -0600

    chore: moving newest to top

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,13 +1,5 @@
 export const APP_CHANGELOG: Record<string, string[]> =
 {
-    "1.3.0": [
-        "Add a setting to send mod mail notifications when a new version of Picture Police is published",
-        "Add a setting to exclude mods from all Picture Police scans",
-        "Reduce false positives on partial matches when only direct Reddit image links are found",
-        "Change all external (non-Reddit) website links to direct-image links",
-        "Fix bug preventing action summaries from being sent",
-        "Change OP whitelist mod menu item to a toggle and add app name to label"
-    ],
     "1.3.1": [
         "Change confidence threshold default setting to 51",
         "Remove unnecessary query strings from direct image URLs",
@@ -16,4 +8,12 @@ export const APP_CHANGELOG: Record<string, string[]> =
         "Add API usage stats to action summaries",
         "Improve matching logic"
     ],
+    "1.3.0": [
+        "Add a setting to send mod mail notifications when a new version of Picture Police is published",
+        "Add a setting to exclude mods from all Picture Police scans",
+        "Reduce false positives on partial matches when only direct Reddit image links are found",
+        "Change all external (non-Reddit) website links to direct-image links",
+        "Fix bug preventing action summaries from being sent",
+        "Change OP whitelist mod menu item to a toggle and add app name to label"
+    ]
 };
